d292aeb667fe7d18fe23962e8d0b1c943b1705cc
Christopher VanZomeren <vanzomerenc@gmail.com>
2014-01-14 22:21:40 -0500

    added bind and lambda functions as modules

---
+++ bind.lua
@@ -0,0 +1,59 @@
+---@module bind
+-- @author Christopher VanZomeren
+-- @copyright (c) 2014 Christopher VanZomeren
+
+assert(..., 'Do not use as main file; use require from different file')
+local _pkg = (...):match("(.-)[^%.]+$")
+local _id = select(2, ...) or ...
+
+
+local error = error
+local select = select
+local type = type
+
+local memoize = require(_pkg..'memoize')
+
+
+local _ENV = {}
+if setfenv then setfenv(1, _ENV) end
+
+
+
+local function bind_at_least_one(f, x, ...)
+
+	local function f_x(...) return f(x, ...) end
+	
+	if select('#', ...) == 0 then return f_x
+	else return bind_at_least_one(f_x, ...)
+	end
+end
+
+bind_at_least_one = memoize(bind_at_least_one, 'allowgc')
+
+
+
+local function bind_n(n, f, ...)
+
+	if select('#', ...) < n then return bind_at_least_one(bind_n, n, f, ...)
+	else return bind_at_least_one(f, ...)
+	end
+end
+
+
+
+local function bind(fst, ...)
+
+	if fst == nil then return bind
+	elseif select('#', ...) == 0 then return bind_at_least_one(bind, fst)
+	elseif type(fst) == 'function' then return bind_at_least_one(fst, ...)
+	elseif type(fst) == 'number' then
+		if fst > 0 then return bind_n(fst, ...)
+		else return select(1, ...)
+		end
+	else error('attempt to bind arguments to non-callable object', 2)
+	end
+end
+
+
+
+return bind
---
+++ lambda.lua
@@ -0,0 +1,80 @@
+---@module lambda
+-- @author Christopher VanZomeren
+-- @copyright (c) 2014 Christopher VanZomeren
+--
+-- @usage
+-- local lambda = require(pkgname..'lambda')
+-- local f = lambda 'x => x^2'
+-- f(3)		-- returns 9
+
+assert(..., 'Do not use as main file; use require from different file')
+local _pkg = (...):match("(.-)[^%.]+$")
+local _id = select(2, ...) or ...
+
+
+local error = error
+local find = string.find
+local gsub = string.gsub
+local load = load
+local loadstring = loadstring
+local pcall = pcall
+local setfenv = setfenv
+local setmetatable = setmetatable
+local type = type
+
+local memoize = require(_pkg..'memoize')
+
+
+local _ENV = {}
+if setfenv then setfenv(1, _ENV) end
+
+
+
+if setfenv and not pcall(load, '') then
+
+	local rawload = load
+
+	function load(ld, source, mode, env)
+	
+		if type(ld) == 'string' then
+	
+			local chunk, err = loadstring(ld, source)
+			if chunk then setfenv(chunk, env) end
+			return chunk, err
+		
+		else
+		
+			return rawload(ld, source, mode, env)
+		end
+	end
+end
+
+
+
+local lambda_env
+do
+	local function lambda_access_error(_, k)
+	
+		error('\''..k..'\' is not accessible from lambda expression', 2)
+	end
+	
+	lambda_env = setmetatable({}, {__index = lambda_access_error, __newindex = lambda_access_error})
+end
+
+
+
+local function lambda(expr)
+
+	if not find(expr, '=>', 1, true) then expr = '_=>'..expr end
+
+	local source = 'return function('..gsub(expr, '=>', ')return ')..' end;'
+	
+	local chunk, err = load(source, expr, 't', lambda_env)
+	if not chunk then error(err, 2) end
+	
+	return chunk()
+end
+
+
+
+return memoize(lambda, 'allowgc', 1)
